78b30c5d35e788428e61d49708b8e21d3e5cd54f
François Ringuette <francois-ringuette@hotmail.com>
2020-03-20 17:20:22 -0400

    Tutorial #7

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import Navbar from './components/Navbar';
 import BookList from './components/BookList';
 import ThemeContextProvider from './contexts/ThemeContext';
 import ThemeToggle from './components/ThemeToggle';
+import { AuthContext } from './contexts/AuthContext';
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <Navbar />
-        <BookList />
-        <ThemeToggle/>
+        <AuthContext.Provider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle />
+        </AuthContext.Provider>
       </ThemeContextProvider>
     </div>
   );
